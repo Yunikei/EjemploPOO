358f1d12df324eed9f5b2255341a4822b66c20ab
Yunkei <alva_alcantar24@unikino.edu.mx>
2024-11-21 07:48:41 -0600

    Agregar archivos de proyecto.

---
+++ EjemploPOO/OperacionesBasicas.cs
@@ -0,0 +1,26 @@
+﻿namespace EjemploPOO
+{
+    internal class OperacionesBasicas
+    {
+        public int Sumar(int primerNumero, int segundoNumero)
+        {
+            return primerNumero + segundoNumero;
+
+        }
+
+        public int Restar(int primerNumero, int segundoNumero)
+        {
+            return (primerNumero - segundoNumero);
+        }
+
+        public int Multiplicar(int primerNumero, int segundoNumero)
+        {
+            return primerNumero * segundoNumero;
+        }
+
+        public decimal Division(int primer, int segundoNumero)
+        {
+            return primer / segundoNumero;
+        }
+    }
+}
---
+++ EjemploPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EjemploPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacionesBasicas basicas = new OperacionesBasicas();
+
+            int resultadoSuma = basicas.Sumar(5, 5);
+            Console.WriteLine(resultadoSuma);
+
+            int resultadoResta = basicas.Restar(18, 8);
+            Console.WriteLine(resultadoResta);
+
+        }
+    }
+}
